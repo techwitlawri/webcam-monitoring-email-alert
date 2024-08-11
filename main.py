e139cd32493750b655d63e28cf2c45bd5f30ddf0
Mmachi Ezeh <mmachilawri@gmail.com>
2024-08-11 16:01:20 +0100

    added a functions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import time
+from emailing import send_email
 
 #to capture video
 video = cv2.VideoCapture(0)
@@ -8,10 +9,12 @@ time.sleep(1)
 
 
 first_frame = None
+status_list = []
+
 
 #to show video
 while True:
-    
+    status = 0
     check, frame = video.read()
     # to convert the frame to grayscale frame
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -29,7 +32,7 @@ while True:
     delta_frame=  cv2.absdiff(first_frame, gray_frame_gau)
     
 
-    thresh_frame= cv2.threshold(delta_frame, 75 ,255,cv2.THRESH_BINARY)[1]
+    thresh_frame= cv2.threshold(delta_frame, 65 ,255,cv2.THRESH_BINARY)[1]
     dil_frame= cv2.dilate(thresh_frame, None, iterations= 2)
     # to  display image or video 
     # that what imshow means, it displayes video
@@ -38,15 +41,26 @@ while True:
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in contours:
-        if cv2.contourArea(contour) < 7000:
+        if cv2.contourArea(contour) < 5000:
             continue
 
         x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x, y),(x+w,y+h),(0,255,0), 3)
+        rectangle= cv2.rectangle(frame, (x, y), (x+w,y+h), (0,255,0), 3)
+        if rectangle.any():
+            status= 1
+            
+            
+    status_list.append(status)
+    status_list = status_list[-2:]
+
+    if status_list[0] == 1 and status_list[1] == 0:
+        send_email()
+
+    print(status_list)
 
     cv2.imshow("video", frame)
     key = cv2.waitKey(1)
-
+ 
 #("q") this is to quit the video, to stop video from displaying.
 # to stop video just press q, to end it.
     if key == ord("q"):
