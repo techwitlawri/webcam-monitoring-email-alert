8f60782cc458512ab3f6e14846847f951c3cb57b
Mmachi Ezeh <mmachilawri@gmail.com>
2024-08-14 20:36:02 +0100

    added function for the images,and smtp for the mail.

--- emailing.py
+++ emailing.py
@@ -1,2 +1,23 @@
-def send_email():
-    print("email was sent")
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+PASSWORD = "tkzg ddsx dvjk pmmu"
+SENDER ="practicecode09@gamil.com"
+RECEIVER = "practicecode09@gamil.com"
+
+
+def send_email(image_path):
+    email_message = EmailMessage
+    email_message["subject"] = "New customer showed up"
+    email_message.set_content("Hey, we just saw a new customer")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+        email_message.add_attachment(content, maintype = "image", subtype= imghdr.what(None, content))
+
+        gmail = smtplib.SMTP("smtp.gmail.com", 587)
+        gmail.ethlo()
+        gmail.starttls()
+        gmail.login(SENDER, PASSWORD)
+        gmail.sendmail(SENDER,RECEIVER, email_message)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2
 import time
+import glob
 from emailing import send_email
+
 
 #to capture video
 video = cv2.VideoCapture(0)
@@ -10,12 +12,17 @@ time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
+
 
 
 #to show video
 while True:
     status = 0
     check, frame = video.read()
+
+
+
     # to convert the frame to grayscale frame
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -48,13 +55,19 @@ while True:
         rectangle= cv2.rectangle(frame, (x, y), (x+w,y+h), (0,255,0), 3)
         if rectangle.any():
             status= 1
+            # to store images
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images= glob.glob("images/*.png")
+            index = int(len(all_images)/ 2)
+            image_with_object= all_images[index]
             
             
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        send_email(image_with_object)
 
     print(status_list)
 
