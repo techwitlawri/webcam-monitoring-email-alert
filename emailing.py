f48b21fe15c478e99a0a11c2e4da3385d8a0cbdd
Mmachi Ezeh <mmachilawri@gmail.com>
2024-08-17 13:46:50 +0100

    made correction of  wrong spelling, removed imghdr, added pillow and io,  editted line 25

--- emailing.py
+++ emailing.py
@@ -1,23 +1,37 @@
 import smtplib
-import imghdr
+#import imghdr
+from PIL import Image
+from io import BytesIO
 from email.message import EmailMessage
 
 PASSWORD = "tkzg ddsx dvjk pmmu"
-SENDER ="practicecode09@gamil.com"
-RECEIVER = "practicecode09@gamil.com"
+SENDER ="practicecode09@gmail.com"
+RECEIVER = "practicecode09@gmail.com"
 
 
 def send_email(image_path):
-    email_message = EmailMessage
-    email_message["subject"] = "New customer showed up"
+    email_message = EmailMessage()
+    email_message["Subject"] = "New customer showed up"
     email_message.set_content("Hey, we just saw a new customer")
 
     with open(image_path, "rb") as file:
         content = file.read()
-        email_message.add_attachment(content, maintype = "image", subtype= imghdr.what(None, content))
+        image_data = BytesIO(content)
+        img = Image.open(image_data)
 
-        gmail = smtplib.SMTP("smtp.gmail.com", 587)
-        gmail.ethlo()
-        gmail.starttls()
-        gmail.login(SENDER, PASSWORD)
-        gmail.sendmail(SENDER,RECEIVER, email_message)
+
+        image_format = img.format
+        print("Image format:", image_format)
+    email_message.add_attachment(content, maintype="image", subtype=image_format)
+    #email_message.add_attachment(content, maintype = "image", subtype= imghdr.what(None, content))
+
+    gmail = smtplib.SMTP( "smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+
+if __name__ == '__main__':
+    send_email(image_path = "images/58.PNG")
+
